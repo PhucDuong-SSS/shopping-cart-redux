610be34e089311ae0e2ed405b76c2fd5ef267086
PhucDuong-SSS <phucdnDev@gmail.com>
2021-01-10 17:00:41 +0700

    productcss

---
+++ src/components/Data.js
@@ -0,0 +1,43 @@
+export const Data = [
+    {
+        _id: 1,
+        img : 'images/img1.jpeg',
+        title : 'nike air max 2090',
+        price : '100',
+        detail : 'nike 2020 is the new latest trend and design from the company with the best material. It is provide the best felling',
+        count : 1,
+        isInCart : false,
+
+    },
+    {
+        _id: 2,
+        img : 'images/img2.jpeg',
+        title : 'nike air max 270',
+        price : '100',
+        detail : 'nike 2020 is the new latest trend and design from the company with the best material. It is provide the best felling',
+        count : 1,
+        isInCart : true,
+
+    },
+    {
+        _id: 3,
+        img : 'images/img3.jpeg',
+        title : 'nike air max 90',
+        price : '100',
+        detail : 'nike 2020 is the new latest trend and design from the company with the best material. It is provide the best felling',
+        count : 1,
+        isInCart : true,
+
+    },
+    {
+        _id: 4,
+        img : 'images/img3.jpeg',
+        title : 'nike air max 90',
+        price : '100',
+        detail : 'nike 2020 is the new latest trend and design from the company with the best material. It is provide the best felling',
+        count : 1,
+        isInCart : true,
+
+    },
+  
+]
